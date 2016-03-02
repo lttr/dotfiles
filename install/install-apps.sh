7fb575cdf4afa69e060744d964f90b4667d3bcfb
Lukas Trumm <lukas.trumm@centrum.cz>
2016-03-02 20:27:32 +0100

    Write install scripts

--- install/install-apps.sh
+++ install/install-apps.sh
@@ -1,26 +1,84 @@
 #!/bin/bash
 
-# Prepare sources
+# ===== Prepare sources =====
 
-## Google Chrome
+# Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
-## Skype
+# Skype
 sudo add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) partner"
 
 
-# Install apt packages
+# ===== Install apt packages =====
 
 sudo apt-get update
 
-for package in $(sed '/^#/d' packages-list.txt); do
-  sudo apt-get install -y $package 
+PACKAGES=(
+	curl
+	dos2unix
+	freemind
+	git
+	gnuplot
+	google-chrome-stable
+	gpick
+	inkscape
+	jabref
+	keepass2
+	krusader
+	libappindicator1
+	libgnome-keyring-dev
+	libindicator7
+	libxss1
+	lyx
+	nautilus-dropbox
+	# neovim
+	nodejs
+	npm
+	openvpn
+	python
+	python3-dev
+	python3-pip
+	python-dbus
+	python-dev
+	python-gtk2
+	python-pip
+	python-setuptools
+	python-wnck
+	python-xlib
+	qstardict
+	silversearcher-ag
+	skype
+	stardict-czech
+	stardict-english-czech
+	tmux
+	trash-cli
+	umlet
+	virtualbox
+	vagrant
+	viking
+	vim-gtk
+	vlc
+	wmctrl
+	xbindkeys
+	xdotool
+	zsh
+)
+
+for package in ${PACKAGES[@]}; do
+  # sudo apt-get install -y $package 
+  echo $package 
 done
 
-# Custom applications
 
-## gsettings-info
+# ===== Custom applications =====
+
+# gsettings-info
 git clone https://github.com/jmatsuzawa/gsettings-info ~/opt/gsettings-info
 ln -s gsettings-info/gsettings-info ~/bin/gsettings-info
+
+
+# ===== Proprietary drivers =====
+
+# Consider configuring drivers in Software & updates -> Drivers
 
--- install/install-symlinks.sh
+++ install/install-symlinks.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
+# ===== Symlink dotfiles =====
+
 # The full path of the parent dir is the dotfiles dir
 cd "$(dirname "$0")/.."
 DOTFILES_ROOT=$(pwd -P)
-
 DOTFILES=(
     #aliases
     #dircolors
@@ -17,18 +20,39 @@ DOTFILES=(
 )
 
 # Backup current dotfiles
-mkdir -p ~/dotfiles_backup
+DOTFILES_BACKUP=~/dotfiles_backup
+mkdir -p $DOTFILES_BACKUP
 
 for dotfile in ${DOTFILES[@]}; do
 	if [[ -e ~/.$dotfile ]]; then
-		cp -R ~/.$dotfile ~/dotfiles_backup
-		echo "$dotfile backuped."
-		rm -rf ~/.$dotfile
+		cp -b ~/.$dotfile $DOTFILES_BACKUP
+		rm ~/.$dotfile
 	fi
 done
 
 # Symlink new dotfiles
 for dotfile in ${DOTFILES[@]}; do
-	ln -s ${DOTFILES_ROOT}/$dotfile ~/.$dotfile
-	echo "Symlink to ${DOTFILES_ROOT}/$dotfile created."
+	ln -sf ${DOTFILES_ROOT}/$dotfile ~/.$dotfile
 done
+
+
+# ===== Symlink application configuration =====
+
+# Krusader
+KRUSADER_FILES=(
+	~/.kde/share/config/krusaderrc
+	~/.kde/share/apps/krusader/krbookmarks.xml
+	~/.kde/share/apps/krusader/krusaderui.rc
+	~/.kde/share/apps/krusader/useractions.xml
+)
+for krusader_file in "${KRUSADER_FILES[@]}"; do
+	if [[ -e "$krusader_file" ]]; then
+		cp -b "$krusader_file" $DOTFILES_BACKUP
+		rm $krusader_file
+	fi
+done
+KRUSADER_CONF=~/Dropbox/conf/krusader
+ln -sf $KRUSADER_CONF/krusaderrc ~/.kde/share/config/krusaderrc 
+ln -sf $KRUSADER_CONF/krbookmarks.xml ~/.kde/share/apps/krusader/krbookmarks.xml 
+ln -sf $KRUSADER_CONF/krusaderui.rc ~/.kde/share/apps/krusader/krusaderui.rc 
+ln -sf $KRUSADER_CONF/useractions.xml ~/.kde/share/apps/krusader/useractions.xml 
