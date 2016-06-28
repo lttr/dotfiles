3a528ad7192229aecf0747f5e8d43637fe7cec51
Lukas Trumm <lukas.trumm@centrum.cz>
2016-06-28 15:28:41 +0200

    Improve script for testing colors

--- colors/test-colors.zsh
+++ colors/test-colors.zsh
@@ -1,7 +1,14 @@
 #!/usr/bin/env zsh
 
+
+msgcat --color=test
+
+echo "======================================================="
+
 autoload -Uz colors && colors
 
+echo "> ZSH arrays with color names test:"
+echo
 echo $fg_no_bold[default]default $fg_bold[default]bold-default
 echo $fg_no_bold[black]black $fg_bold[black]bold-black
 echo $fg_no_bold[blue]blue $fg_bold[blue]bold-blue
@@ -12,3 +19,16 @@ echo $fg_no_bold[red]red $fg_bold[red]bold-red
 echo $fg_no_bold[white]white $fg_bold[white]bold-white
 echo $fg_no_bold[yellow]yellow $fg_bold[yellow]bold-yellow
 echo $fg_no_bold[default]
+
+# OR
+
+
+echo "> Terminal number definition of colors test:"
+echo
+for i in {0..9}; do
+    echo -n ${$(tput setaf $i)}test $i ${$(tput sgr 0)}; 
+    echo -n ${$(tput bold)}${$(tput setaf $i)}test $i ${$(tput sgr 0)}; 
+    echo -n ${$(tput smul)}${$(tput setaf $i)}test $i ${$(tput sgr 0)}; 
+    echo -n ${$(tput smso)}${$(tput setaf $i)}test $i ${$(tput sgr 0)}; 
+    echo
+done
