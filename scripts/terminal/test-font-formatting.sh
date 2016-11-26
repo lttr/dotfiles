60277e4a27862f1e863c1f62add8d55eb6003337
Lukas Trumm <lukas.trumm@centrum.cz>
2016-11-26 07:28:11 +0100

    Add terminal scripts

---
+++ scripts/terminal/test-font-formatting.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo -e "\e[0mNormal Text"
+echo -e "Normal \e[1mBold         \e[21mNormal"
+echo -e "Normal \e[3mitalic\e[0m       \e[21mNormal"
+echo -e "Normal \e[2mDim          \e[22mNormal"
+echo -e "Normal \e[4mUnderlined   \e[24mNormal"
+echo -e "Normal \e[5mBlink        \e[25mNormal"
+echo -e "Normal \e[7minverted     \e[27mNormal"
+echo -e "Normal \e[8mHidden       \e[28mNormal"
