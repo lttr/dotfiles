f420b60c4dc0d8cccb812a8acc32d5cfcacc9ecf
Lukas Trumm <lukas.trumm@centrum.cz>
2018-01-19 13:47:33 +0100

    Improve WSL integration

--- scripts/wsl/terminal.sh
+++ scripts/wsl/terminal.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+DISPLAY=:0 xrdb -load ~/.Xresources
 DISPLAY=:0 urxvtc -e sh -c "zsh" "$@"
-if [ $? \-eq 2 ]; then
-	urxvtd \-q \-o \-f
+if [[ $? -eq 2 ]]; then
+	urxvtd -q -o -f
 	DISPLAY=:0 urxvtc -e sh -c "zsh" "$@"
 fi
