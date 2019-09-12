ec8e9d07dfbc1507d1e459e91b182b6678d58607
Lukas Trumm <lukas.trumm@centrum.cz>
2019-09-12 13:05:06 +0200

    Improve web-start script

--- scripts/web/web-start.sh
+++ scripts/web/web-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-name=$1
+name=${1:-"web-sandbox_`date +%Y-%m-%d_%H:%M`"}
 
 browser_exe="firefox"
 browser_name="Mozilla Firefox"
@@ -8,48 +8,46 @@ editor_exe="code"
 editor_name="Visual Studio Code"
 port="3003"
 
-if [ -z $name ]; then
-  echo 'Missing sandbox name'
-else
-  cd ~/sandbox
+cd ~/sandbox
 
-  command -v pollinate >/dev/null 2>&1 || { \
-    echo >&2 "I require 'pollinate' but it's not installed. Aborting."; exit 1; \
-  }
+command -v pollinate >/dev/null 2>&1 || { \
+  echo >&2 "I require 'pollinate' but it's not installed. Aborting."; exit 1; \
+}
 
-  pollinate ~/code/web-start --name $name
+pollinate ~/code/web-start --name $name
 
-  cd $name
+cd $name
 
-  git init
-  git add .
-  git commit -m "Initial commit"
+git init
+git add .
+git commit -m "Initial commit"
 
-  # get width of screen and height of screen
-  SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
-  SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
-  # new width and height
-  W=$(( $SCREEN_WIDTH / 2 ))
-  H=$(( $SCREEN_HEIGHT ))
-  # moving to the right half of the screen:
-  X1=0
-  X2=$(( $SCREEN_WIDTH / 2 ))
-  Y=0
+# get width of screen and height of screen
+SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
+SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
+# new width and height
+W=$(( $SCREEN_WIDTH / 2 ))
+H=$(( $SCREEN_HEIGHT ))
+# moving to the right half of the screen:
+X1=0
+X2=$(( $SCREEN_WIDTH / 2 ))
+Y=0
 
-  echo "Moving $editor_name to position $X1,$Y and resizing to $W,$H"
-  echo "Moving $browser_name to position $X2,$Y and resizing to $W,$H"
+echo "Moving $editor_name to position $X1,$Y and resizing to $W,$H"
+echo "Moving $browser_name to position $X2,$Y and resizing to $W,$H"
 
-  bash -c "sleep 1; $browser_exe -new-tab http://localhost:$port" &
+bash -c "sleep 3; $browser_exe -new-tab http://localhost:$port" &
 
-  $editor_exe .
-  $editor_exe --reuse-window index.html style.css script.js
+$editor_exe .
+$editor_exe --reuse-window index.html style.css script.js
 
-  sleep 2
+sleep 2
 
-  wmctrl -r "$browser_name" -b remove,maximized_vert,maximized_horz \
-    && wmctrl -r "$browser_name" -e 0,$X2,$Y,$W,$H
-  wmctrl -r "$name - $editor_name" -b remove,maximized_vert,maximized_horz \
-    && wmctrl -r "$name - $editor_name" -e 0,$X1,$Y,$W,$H
+wmctrl -r "$browser_name" -b remove,maximized_vert,maximized_horz \
+  && wmctrl -r "$browser_name" -e 0,$X2,$Y,$W,$H
+wmctrl -r "$name - $editor_name" -b remove,maximized_vert,maximized_horz \
+  && wmctrl -r "$name - $editor_name" -e 0,$X1,$Y,$W,$H
 
-  browser-sync start --server --files . --no-notify --browser $browser_exe --port $port --no-open
-fi
+bash -c "sleep 2;wmctrl -ia `wmctrl -lx | grep "$name - $editor_name" | awk '{print $1}'`"
+
+browser-sync start --server --files . --no-notify --browser $browser_exe --port $port --no-open
