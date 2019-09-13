a25e5044c7ddd64210efa0ccad739ebe2b744719
Lukas Trumm <lukas.trumm@centrum.cz>
2019-09-13 09:26:28 +0200

    Fix github script

--- scripts/git/my-github-js-projects.sh
+++ scripts/git/my-github-js-projects.sh
@@ -8,11 +8,11 @@ REPOS=`curl -s https://api.github.com/users/${USER}/repos`
 REPOS_LIST=`echo "$REPOS" | json -c 'this.fork != true' -a name`
 
 echo $REPOS_LIST | while read REPO ; do
-  RESPONSE=`curl -s "https://api.github.com/repos/lttr/${REPO_NAME}/contents/${LOOKING_FOR}"`
+  RESPONSE=`curl -s "https://api.github.com/repos/lttr/${REPO}/contents/${LOOKING_FOR}"`
   echo $RESPONSE | grep -q 'Not Found'
   if [ $? -ne 0 ]; then
     echo "[${REPO}]"
-    echo $RESPONSE | json content | base64 -d | json name version
+    echo $RESPONSE | tr -d '\n' | json content | base64 -d | json name version description
+    echo
   fi
-  echo
 done
