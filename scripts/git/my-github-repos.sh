1c5867bc244df61adf6f55f34a8aea461c76819d
Lukas Trumm <lukas.trumm@centrum.cz>
2019-11-10 14:52:25 +0100

    Improved my github repos script

--- scripts/git/my-github-repos.sh
+++ scripts/git/my-github-repos.sh
@@ -2,9 +2,16 @@
 
 USER='lttr'
 
+mREPOS=$(curl -s https://api.github.com/users/${USER}/repos)
+
 echo '[Public repos]'
-curl -s https://api.github.com/users/${USER}/repos | json -c 'this.fork != true' -a name
+echo $REPOS | json -c 'this.archived != true && this.fork != true' -e 'this.push = this.pushed_at.substring(0,10)' -a '.push' '.name' | sort -r
 
 echo
 echo '[Forked repos]'
-curl -s https://api.github.com/users/${USER}/repos | json -c 'this.fork == true' -a name
+echo $REPOS | json -c 'this.fork == true' -e 'this.push = this.pushed_at.substring(0,10)' -a '.push' '.name' | sort -r
+
+echo
+echo '[Archived repos]'
+echo $REPOS | json -c 'this.archived == true' -e 'this.push = this.pushed_at.substring(0,10)' -a '.push' '.name' | sort -r
+
