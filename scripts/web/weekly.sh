8bb5271538c696ba30499a0eade0207cf841a82f
Lukas Trumm <lukas.trumm@centrum.cz>
2019-11-27 08:49:04 +0100

    Change weekly to use chrome

--- scripts/web/weekly.sh
+++ scripts/web/weekly.sh
@@ -1,11 +1,8 @@
-BROWSER=firefox
+BROWSER=google-chrome
 
-$BROWSER -new-window https://drive.google.com/drive/u/3/folders/0B604VDf_fxQzTTVPdFpfZ0V6V3M
-sleep 0.2
-$BROWSER -new-tab https://app.simplenote.com/
-sleep 0.2
-$BROWSER -new-tab https://www.wunderlist.com/#/lists/inbox
-sleep 0.2
-$BROWSER -new-tab https://mail.google.com/mail/u/3/#inbox
-sleep 0.2
-$BROWSER -new-tab https://calendar.google.com/calendar/b/3/r?pli=1
+$BROWSER --new-window \
+-d https://drive.google.com/drive/u/3/folders/0B604VDf_fxQzTTVPdFpfZ0V6V3M \
+-d https://app.simplenote.com/ \
+-d https://www.wunderlist.com/#/lists/inbox \
+-d https://mail.google.com/mail/u/3/#inbox \
+-d https://calendar.google.com/calendar/b/3/r?pli=1
