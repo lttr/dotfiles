a68d0225ef84c5ecb12937e0936ed2c0ac0475a6
Lukas Trumm <lukas.trumm@centrum.cz>
2020-01-05 09:30:11 +0100

    Do not print archived forked repos

--- scripts/git/my-github-repos.sh
+++ scripts/git/my-github-repos.sh
@@ -2,14 +2,14 @@
 
 USER='lttr'
 
-mREPOS=$(curl -s https://api.github.com/users/${USER}/repos)
+REPOS=$(curl -s https://api.github.com/users/${USER}/repos)
 
 echo '[Public repos]'
 echo $REPOS | json -c 'this.archived != true && this.fork != true' -e 'this.push = this.pushed_at.substring(0,10)' -a '.push' '.name' | sort -r
 
 echo
 echo '[Forked repos]'
-echo $REPOS | json -c 'this.fork == true' -e 'this.push = this.pushed_at.substring(0,10)' -a '.push' '.name' | sort -r
+echo $REPOS | json -c 'this.fork == true && this.archived != true' -e 'this.push = this.pushed_at.substring(0,10)' -a '.push' '.name' | sort -r
 
 echo
 echo '[Archived repos]'
