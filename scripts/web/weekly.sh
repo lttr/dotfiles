2a67273d2acbb8f08bdedd52ae7aa4c745f41458
Lukas Trumm <lukas.trumm@centrum.cz>
2020-01-20 11:38:09 +0100

    Swich from wunderlist to ticktick

--- scripts/web/weekly.sh
+++ scripts/web/weekly.sh
@@ -3,6 +3,6 @@ BROWSER=google-chrome
 $BROWSER --new-window \
 -d https://drive.google.com/drive/u/3/folders/0B604VDf_fxQzTTVPdFpfZ0V6V3M \
 -d https://app.simplenote.com/ \
--d https://www.wunderlist.com/#/lists/inbox \
+-d https://ticktick.com \
 -d https://mail.google.com/mail/u/3/#inbox \
 -d https://calendar.google.com/calendar/b/3/r?pli=1
