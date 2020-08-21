578779acd94157605064ba15bd3b04a993b621f1
Lukas Trumm <lukas.trumm@centrum.cz>
2020-08-21 13:34:33 +0200

    Pin again

--- bootstrap/install/deps.ts
+++ bootstrap/install/deps.ts
@@ -1,16 +1,16 @@
 // std
-import * as fs from 'https://deno.land/std/fs/mod.ts@0.65'
-import * as log from 'https://deno.land/std/log/mod.ts@0.65'
-import * as path from 'https://deno.land/std/path/mod.ts@0.65'
+import * as fs from 'https://deno.land/std/fs/mod.ts@0.65.0'
+import * as log from 'https://deno.land/std/log/mod.ts@0.65.0'
+import * as path from 'https://deno.land/std/path/mod.ts@0.65.0'
 
 export { fs }
 export { log }
 export { path }
 
-export { ensureSymlink } from 'https://deno.land/std/fs/ensure_symlink.ts@0.65'
-export { getFileInfoType } from 'https://deno.land/std/fs/_util.ts@0.65'
-export { ensureDir } from 'https://deno.land/std/fs/ensure_dir.ts@0.65'
-export { exists } from 'https://deno.land/std/fs/exists.ts@0.65'
+export { ensureSymlink } from 'https://deno.land/std/fs/ensure_symlink.ts@0.65.0'
+export { getFileInfoType } from 'https://deno.land/std/fs/_util.ts@0.65.0'
+export { ensureDir } from 'https://deno.land/std/fs/ensure_dir.ts@0.65.0'
+export { exists } from 'https://deno.land/std/fs/exists.ts@0.65.0'
 
 // user land
 
