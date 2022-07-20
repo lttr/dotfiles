91f62bcf1fb41e2ed2e2cbf78ff22d67f0c045c1
Lukas Trumm <lukas.trumm@gmail.com>
2022-07-20 13:52:57 +0200

    Move web-start script into terminal workflow

--- scripts/web/web-start.sh
+++ scripts/web/web-start.sh
@@ -4,50 +4,66 @@ name=${1:-"web-sandbox_`date +%Y-%m-%d_%H:%M`"}
 
 browser_exe="firefox"
 browser_name="Mozilla Firefox"
-editor_exe="code"
-editor_name="Visual Studio Code"
+editor_exe="nvim"
+editor_name="neovim"
 port="3003"
 
 cd ~/sandbox
 
-command -v pollinate >/dev/null 2>&1 || { \
-  echo >&2 "I require 'pollinate' but it's not installed. Aborting."; exit 1; \
-}
+# Initialize a project if it does not exists
+if [ -d "$name" ]
+then
+  echo "Project '$name' already exists"
+  cd $name
+else
+  # https://www.npmjs.com/package/pollinate
+  command -v pollinate >/dev/null 2>&1 || { \
+    echo >&2 "I require 'pollinate' but it's not installed. Aborting."; exit 1; \
+  }
 
-pollinate ~/code/web-start --name $name
+  pollinate ~/code/web-start --name $name >/dev/null
 
-cd $name
+  cd $name
 
-git init
-git add .
-git commit -m "Initial commit"
+  git init >/dev/null
+  git add .
+  git commit -m "Initial commit"
+fi
 
-# get width of screen and height of screen
-SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
-SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
-# new width and height
-W=$(( $SCREEN_WIDTH / 2 ))
-H=$(( $SCREEN_HEIGHT ))
-# moving to the right half of the screen:
-X1=0
-X2=$(( $SCREEN_WIDTH / 2 ))
-Y=0
+# launch editor
+bash -c "kitty @ launch --type tab --cwd ~/sandbox/${name} nvim index.html" &
 
-echo "Moving $editor_name to position $X1,$Y and resizing to $W,$H"
-echo "Moving $browser_name to position $X2,$Y and resizing to $W,$H"
+# launch dev server
+browser-sync start --server --files . --no-notify --browser $browser_exe --port $port
 
-bash -c "sleep 3; $browser_exe -new-tab http://localhost:$port" &
+# Next is some prototype of auto layout (editor to the left, browser to the right)
+# However much better is to rely on the window manager to do that
+# (currently I use PopOS extention for tiling windows with Win+y shortcut)
 
-$editor_exe .
-$editor_exe --reuse-window index.html style.css script.js
+# # get width of screen and height of screen
+# SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
+# SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
+# # new width and height
+# W=$(( $SCREEN_WIDTH / 2 ))
+# H=$(( $SCREEN_HEIGHT ))
+# # moving to the right half of the screen:
+# X1=0
+# X2=$(( $SCREEN_WIDTH / 2 ))
+# Y=0
 
-sleep 2
+# echo "Moving $editor_name to position $X1,$Y and resizing to $W,$H"
+# echo "Moving $browser_name to position $X2,$Y and resizing to $W,$H"
 
-wmctrl -r "$browser_name" -b remove,maximized_vert,maximized_horz \
-  && wmctrl -r "$browser_name" -e 0,$X2,$Y,$W,$H
-wmctrl -r "$name - $editor_name" -b remove,maximized_vert,maximized_horz \
-  && wmctrl -r "$name - $editor_name" -e 0,$X1,$Y,$W,$H
+# launch browser
+# bash -c "$browser_exe -new-tab http://localhost:$port" &
 
-bash -c "sleep 2;wmctrl -ia `wmctrl -lx | grep "$name - $editor_name" | awk '{print $1}'`"
 
-browser-sync start --server --files . --no-notify --browser $browser_exe --port $port --no-open
+# sleep 2
+
+# wmctrl -r "$browser_name" -b remove,maximized_vert,maximized_horz \
+#   && wmctrl -r "$browser_name" -e 0,$X2,$Y,$W,$H
+# wmctrl -r "$name - $editor_name" -b remove,maximized_vert,maximized_horz \
+#   && wmctrl -r "$name - $editor_name" -e 0,$X1,$Y,$W,$H
+
+# bash -c "sleep 2;wmctrl -ia `wmctrl -lx | grep "$name - $editor_name" | awk '{print $1}'`"
+
