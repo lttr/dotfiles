8755b742ab5b1753d42e5d2e83b49c72b03fb344
Lukas Trumm <lukas.trumm@gmail.com>
2022-11-25 11:43:08 +0100

    Clone repos script

---
+++ scripts/git/clone-my-repos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+USER='lttr'
+CODE_DIR='~/code'
+
+REPOS=$(gh api users/lttr/repos --paginate --jq '.[] | select(.archived != true) | select(.fork != true) | .ssh_url')
+
+cd $CODE_DIR
+
+for URL in $REPOS; do
+  git clone $URL
+end
+
