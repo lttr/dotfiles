597576ed32840d5f74ddac704fdd603584f74057
Lukas Trumm <lukas.trumm@gmail.com>
2022-11-25 12:12:32 +0100

    chore

--- scripts/git/clone-my-repos.sh
+++ scripts/git/clone-my-repos.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
-USER='lttr'
+GH_USER='lttr'
 CODE_DIR="$HOME/code"
 
-REPOS=$(gh api users/${USER}/repos --paginate --jq '.[] | select(.archived != true) | select(.fork != true) | .ssh_url')
+REPOS=$(gh api users/${GH_USER}/repos --paginate --jq '.[] | select(.archived != true) | select(.fork != true) | .ssh_url')
 
 cd $CODE_DIR
 
 for URL in $REPOS; do
-  git clone $URL
+  git clone "$URL"
 done
 
