2d6c2370f86710344174741cb89aa121c1898a56
Lukas Trumm <lukas.trumm@gmail.com>
2025-06-14 13:35:38 +0200

    Improve render-markdown config

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -6,12 +6,16 @@ return {
   opts = {
     preset = "obsidian",
     heading = {
-
-      code = {
-        sign = false,
-        left_pad = 3,
-        right_pad = 3,
-      },
+      icons = {}, -- display normal # signs instead of icons
+      sign = false,
+    },
+    code = {
+      sign = false,
+      left_pad = 3,
+      right_pad = 3,
+      inline_pad = 1,
+      border = "thick", -- Background covers leading and trailing ```
+      style = "normal", -- No language indicator
     },
   },
 }
